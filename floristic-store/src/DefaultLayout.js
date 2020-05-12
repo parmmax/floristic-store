ff8ecfb62dd142a890ca4ae18753aaf6f5212908
Maxim Parmakly <58299254+parmmax@users.noreply.github.com>
2020-05-13 01:11:57 +0300

    scrollFunction
    header scroll animation

--- floristic-store/src/DefaultLayout.js
+++ floristic-store/src/DefaultLayout.js
@@ -1,64 +1,85 @@
-import React from "react";
-import "./DefaultLayout.scss"
-import "./styles/fonts.scss"
-import "./styles/reset.scss"
+import React from 'react';
+import classes from './DefaultLayout.module.scss'
+
+import './assets/fonts.scss'
+import './assets/reset.scss'
 
 // debugger;
-//код из примера по анимации
-// -------------------------------------------------------------------------------------------------------
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+        document.getElementById("menu").style.height = "80px";
+    } else {
+        document.getElementById("menu").style.height = "160px";
+    }
+}
 
-//конец кода из примера по анимации
-//--------------------------------------------------------------------------------------------------------
 
 const DefaultLayout = () => {
     return (
-        <div className="defaultLayout">
-            <header>Header</header>
-            <main className="mainContainer">
+        <div className={classes.defaultLayout}>
+            <nav id="menu" className={classes.menu}>
+                <ul className={classes.list}>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">PRODUCTS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">CONTACTS</a></li>
+                </ul>
+            </nav>
+
+            <main className={classes.mainContainer}>
                 <h1>Main Content</h1>
-                <section className="firstSection">
+                <section className={classes.firstSection}>
                     <h2>First section</h2>
                     <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consequuntur cum, cumque
                         debitis deleniti dolores ducimus eligendi eos et eveniet excepturi explicabo facilis fugit
-                        incidunt iusto laborum minima molestiae nam natus nisi non officiis omnis perferendis quaerat
-                        quam quasi quis quod quos recusandae reprehenderit repudiandae saepe sapiente similique unde vel
+                        incidunt iusto laborum minima molestiae nam natus nisi non officiis omnis perferendis
+                        quaerat
+                        quam quasi quis quod quos recusandae reprehenderit repudiandae saepe sapiente similique unde
+                        vel
                         veritatis vitae. Architecto eos necessitatibus obcaecati officiis pariatur, rem totam.
                     </div>
                 </section>
-                <section className="secondSection">
+                <section className={classes.secondSection}>
                     <h2>Second section</h2>
                     <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium dicta illo vero
                         voluptatum! Asperiores debitis dicta, error inventore labore nemo, quaerat quam quia quis,
                         recusandae sapiente sint? At aut dignissimos doloribus ex explicabo iste laudantium magnam
                         maxime mollitia natus nisi, obcaecati odit placeat repellendus repudiandae saepe sapiente,
-                        tempora. Accusantium beatae dicta dolorum excepturi explicabo fugit illo impedit laborum libero
+                        tempora. Accusantium beatae dicta dolorum excepturi explicabo fugit illo impedit laborum
+                        libero
                         maiores minima minus natus nemo optio sequi similique, soluta tenetur velit!
                     </div>
                 </section>
-                <section className="thirdSection" >
+                <section className={classes.thirdSection}>
                     <h2>Third section</h2>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aperiam debitis, est expedita
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aperiam debitis, est
+                        expedita
                         magni obcaecati omnis quae, quo tenetur totam vel velit! Ab adipisci aliquam aliquid beatae
-                        cumque deleniti dicta eius harum ipsam maiores minus modi mollitia nesciunt obcaecati porro quas
+                        cumque deleniti dicta eius harum ipsam maiores minus modi mollitia nesciunt obcaecati porro
+                        quas
                         qui, quos recusandae sequi tempore tenetur velit voluptatem voluptatum? Accusamus, aliquid
                         corporis debitis error eveniet expedita explicabo facere fugit laboriosam libero nisi non
                         nostrum obcaecati odio pariatur repellendus ullam.
                     </div>
                 </section>
-
-                <section className="fourthSection">
+                <section className={classes.fourthSection}>
                     <h2>Fourth section</h2>
-                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos eius eligendi,
-                        eos est exercitationem fugiat illo, magnam neque nobis nulla possimus quae quo rem voluptatum?
-                        Iure maiores pariatur quas sequi voluptates. Deleniti fugiat ipsa odit provident reprehenderit
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dignissimos eius
+                        eligendi,
+                        eos est exercitationem fugiat illo, magnam neque nobis nulla possimus quae quo rem
+                        voluptatum?
+                        Iure maiores pariatur quas sequi voluptates. Deleniti fugiat ipsa odit provident
+                        reprehenderit
                         veniam. Alias, aperiam, ducimus eveniet in inventore itaque iusto, molestiae nobis quia quod
-                        suscipit veniam? Amet atque aut autem, cupiditate dolorum est ex, expedita iure, nulla placeat
+                        suscipit veniam? Amet atque aut autem, cupiditate dolorum est ex, expedita iure, nulla
+                        placeat
                         repudiandae voluptatem voluptatum. Cumque, excepturi.
                     </div>
                 </section>
-
-                <section className="fifthSection">
+                <section className={classes.fifthSection}>
                     <h2>Fifth section</h2>
                     <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid dolor excepturi, harum
                         minima placeat provident quis quo ratione sapiente sed temporibus. Alias animi atque cum
@@ -69,7 +90,8 @@ const DefaultLayout = () => {
                     </div>
                 </section>
             </main>
-            <footer className="layoutFooter">Footer</footer>
+
+            <footer className={classes.layoutFooter}>Footer</footer>
         </div>
     )
 };
